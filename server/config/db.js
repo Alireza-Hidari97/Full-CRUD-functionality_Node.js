9648322123b66fb73d46067df92b874d353bc055
Alireza Haidari <m.ghorbat@yahoo.com>
2021-10-28 15:42:46 -0400

    files for heruco

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://Mid-Term:37ItFjaHcQGnXBc8@cluster0.zq8iu.mongodb.net/books229?retryWrites=true&w=majority"
+};
